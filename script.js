ceb79088d8f2bf9150fd02fb88470a0c70ccf8e2
amalali8 <amalza958@gmail.com>
2023-08-12 20:14:09 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Good Luck 💪🏾
+  function mostExpensive(jewellery) {
+    let maxPrice = 0;
+    let maxjewellery = "";
+  
+    for (let piece in jewellery) {
+      if (jewellery[piece] > maxPrice) {
+        maxPrice = jewellery[piece];
+        maxjewellery = piece;
+      }
+    }
+  
+    return `The most expensive one is the ${maxjewellery}`;
+  }
+  
+  const j1 = {
+    "Diamond Earrings": 1180,
+    "Gold Watch": 210,
+    "Pearl Necklace": 737
+  };
+  console.log(mostExpensive(j1));
+  
+  const j2 = {
+    "Silver Bracelet": 280,
+    "Gemstone Earrings": 180,
+    "Diamond Ring": 3500
+  };
+  console.log(mostExpensive(j2));
+  
+  
